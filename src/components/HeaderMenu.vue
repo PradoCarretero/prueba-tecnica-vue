bd0569d5c18f727103525b6d58372842a2b47215
PradoCarretero <p.carreteroayala@gmail.com>
2022-10-19 12:39:14 +0200

    tailwind working

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -1,11 +1,17 @@
 <template>
-  <header>
-    <h1>MealFlix</h1>
+  <header class="container mx-auto">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1>Hello world!</h1>
+    <section class="container mx-auto">
+      <router-link to="/" class="container mx-auto">
+        <img src="../assets/logo.png" alt="" width="90px" />
+      </router-link>
+    </section>
     <nav>
       <router-link to="/">Comidas</router-link> |
       <router-link to="/favoritos">Favoritos</router-link>
     </nav>
-    <img src="" alt="" />
+    <i class="fa-solid fa-circle-h fa-xl"></i>
   </header>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import store from "./store.js";
-import router from "./router";
 import VueAxios from "vue-axios";
 import Axios from "axios";
+import router from "./router";
+import store from "./store.js";
+import "./input.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, Axios);
