1717f241c633b8ef8e305c2de038ebb5c72cee39
PradoCarretero <p.carreteroayala@gmail.com>
2022-10-20 12:59:45 +0200

    add transparency

--- src/components/RecipeWindow.vue
+++ src/components/RecipeWindow.vue
@@ -1,16 +1,18 @@
 <template>
-  <section
-    class="modal-overlay absolute top-0 right-0 bottom-0 left-0 bg-white opacity-90 z-10"
+  <div
+    class="modal-overlay absolute top-0 right-0 bottom-0 left-0 bg-[rgba(255,255,255,0.90)] Fuente: https://www.iteramos.com/pregunta/12417/no-quiero-heredar-la-opacidad-del-hijo-del-padre-en-css] z-10 flex items-center justify-center"
   >
-    <h2>Receta</h2>
-    <p>{{ strInstructions }}</p>
-  </section>
+    <section class="w-5/12 min-h-[41.6%] bg-white relative p-12">
+      <i class="fa-regular fa-circle-xmark absolute top-0 right-0 m-2"></i>
+      <h2 class="text-3xl text-center mb-12">Receta</h2>
+      <p class="text-xl">{{ strInstructions }}</p>
+    </section>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-/* import FoodList from "../components/FoodList.vue";
- */
+
 export default {
   name: "RecipeWindow",
 
@@ -22,6 +24,5 @@ export default {
       return this.$store.state.showModal.strInstructions;
     },
   },
-  /*   components: { FoodList }, */
 };
 </script>
